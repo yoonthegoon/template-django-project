2677c71a86382cc6ab711f84cbdb0955e2100a13
yoonthegoon <71526721+yoonthegoon@users.noreply.github.com>
2023-02-14 10:36:31 -0500

    add pre-commit

--- template_django_app/apps.py
+++ template_django_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TemplateDjangoAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'template_django_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "template_django_app"
